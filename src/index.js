5b69b2927c7172f936a96bc389657606cfc0eb03
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-10-08 11:26:14 +0600

    new-home design started

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home';
+// import Home from './pages/Home';
 import './assets/css/style.css'
+import NewHome from './pages/New-home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Home/>
+  <NewHome/>
 );
---
+++ src/pages/New-home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewHome = () => {
+    return (
+        <div>
+            New Home
+        </div>
+    )
+}
+
+export default NewHome
